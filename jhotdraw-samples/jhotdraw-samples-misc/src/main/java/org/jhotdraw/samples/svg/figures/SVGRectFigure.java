dd3783094a58525067066df40633cd5f021b137a
Kasper Sørensen <kaspersorensen1988@gmail.com>
2023-12-30 15:09:10 +0100

    Added feature entry points related to editting a rectangle

--- jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/svg/figures/SVGRectFigure.java
+++ jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/svg/figures/SVGRectFigure.java
@@ -10,6 +10,8 @@ package org.jhotdraw.samples.svg.figures;
 import java.awt.*;
 import java.awt.geom.*;
 import java.util.*;
+
+import dk.sdu.mmmi.featuretracer.lib.FeatureEntryPoint;
 import org.jhotdraw.draw.*;
 import static org.jhotdraw.draw.AttributeKeys.FILL_COLOR;
 import static org.jhotdraw.draw.AttributeKeys.STROKE_CAP;
@@ -59,6 +61,7 @@ public class SVGRectFigure extends SVGAttributedFigure implements SVGFigure {
     }
     /**
      */
+
     private RoundRectangle2D.Double roundrect;
     /**
      * This is used to perform faster drawing.
@@ -72,6 +75,7 @@ public class SVGRectFigure extends SVGAttributedFigure implements SVGFigure {
     /**
      * Creates a new instance.
      */
+
     public SVGRectFigure() {
         this(0, 0, 0, 0);
     }
@@ -323,6 +327,7 @@ public class SVGRectFigure extends SVGAttributedFigure implements SVGFigure {
     }
 
     // EDITING
+    @FeatureEntryPoint(value = "Create handles")
     @Override
     public Collection<Handle> createHandles(int detailLevel) {
         LinkedList<Handle> handles = new LinkedList<Handle>();
