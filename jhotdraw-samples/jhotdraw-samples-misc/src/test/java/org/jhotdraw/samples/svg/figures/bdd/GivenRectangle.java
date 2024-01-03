7e2b2fd70fb922efd3ae048be3252eb6679ff492
Kasper <kaspersorensen1988@gmail.com>
2024-01-03 19:13:33 +0100

    BDD done

---
+++ jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/figures/bdd/GivenRectangle.java
@@ -0,0 +1,19 @@
+package org.jhotdraw.samples.svg.figures.bdd;
+
+import com.tngtech.jgiven.Stage;
+import org.jhotdraw.samples.svg.figures.SVGRectFigure;
+
+public class GivenRectangle extends Stage<GivenRectangle> {
+
+    private SVGRectFigure svgRectFigure;
+
+    public GivenRectangle rectangleDrawnOnTheCanvas() {
+        svgRectFigure = new SVGRectFigure(0,0,100,100);
+
+        return self();
+    }
+
+    public SVGRectFigure getSvgRectFigure(){
+        return svgRectFigure;
+    }
+}
